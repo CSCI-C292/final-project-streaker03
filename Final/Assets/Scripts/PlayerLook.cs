ceff4f8309c28ee7d951e2532c45b3affaf4a1e5
streaker03 <andrewgostomelsky@gmail.com>
2020-11-12 14:32:58 -0500

    Player Look and Movement

---
+++ Final/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    public Camera cam;
+    public float lookSpeedX;
+    public float lookSpeedY;
+    public float upperYClamp;
+    public float lowerYClamp;
+
+    private float currentY;
+    
+    // Start is called before the first frame update
+    void Start() {
+        currentY = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(Vector3.up, Input.GetAxisRaw("Mouse X") * lookSpeedX * Time.deltaTime);
+        currentY -= Input.GetAxisRaw("Mouse Y") * lookSpeedY * Time.deltaTime;
+        currentY = Mathf.Clamp(currentY, upperYClamp * -1, lowerYClamp);
+        cam.transform.localRotation = Quaternion.Euler(currentY, 0f, 0f);
+
+
+    }
+}
